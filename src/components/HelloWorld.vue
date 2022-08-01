3c6fbc0e7e0bd4bd31dfcdded0bfbee139d2cfb5
sclee92 <sclee92@e8ight.co.kr>
2022-08-01 11:43:29 +0900

    asd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>this is jenkins test</h2>
-    <h5>iq90z98190s</h5>
+    <h5>zxcvasdasd</h5>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
